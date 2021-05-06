32fe74918ee84e25458f78a6145b2650940fe597
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-05-06 12:54:33 -0400

    Added a footer sction to the index.html file.

---
+++ index.js
@@ -0,0 +1,13 @@
+function paymentMethod (){
+
+    alert("Please provide a payment method");
+
+    
+}
+
+let button= document.getElementById("#nav-donate");
+button.addEventListener("click", paymentMethod)
+
+var instance = M.Carousel.init({
+    fullWidth: true
+  });
