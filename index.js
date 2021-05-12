17ac3f67c3e6dfba3513595739c2ddb6d216e890
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-05-11 20:01:57 -0400

    Added an immediate function for the button.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+// added an immediate executing function
 function paymentMethod (){
 
-    alert("Please provide a payment method");
-
+    const stripe=stripe('pk_test_51Iq55tDcRN3IA7cEHllozwTPRlBlFDC1c9ipzOew8gHLKSBRCDxoT4lhhm3ndTJOK1zYaoZFgN5PFAHVuJvL6JSJ00siu7wyls')
     
 }
 
 let button= document.getElementById("#nav-donate");
 button.addEventListener("click", paymentMethod)
 
-var instance = M.Carousel.init({
-    fullWidth: true
-  });
+
+
