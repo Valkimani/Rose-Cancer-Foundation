43033694efd2605874e77aedbf550ce5956d8956
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-05-11 20:07:01 -0400

    Added a get element by ID for thr button and an error message.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // added an immediate executing function
 function paymentMethod (){
-
-    const stripe=stripe('pk_test_51Iq55tDcRN3IA7cEHllozwTPRlBlFDC1c9ipzOew8gHLKSBRCDxoT4lhhm3ndTJOK1zYaoZFgN5PFAHVuJvL6JSJ00siu7wyls')
+    // Takes in publishable key
+    const stripe = stripe('pk_test_51Iq55tDcRN3IA7cEHllozwTPRlBlFDC1c9ipzOew8gHLKSBRCDxoT4lhhm3ndTJOK1zYaoZFgN5PFAHVuJvL6JSJ00siu7wyls');
     
+    const checkoutButton =  document.getElementById('nav-button');
+    const errorMessage = document.getElementById('error-message');
+
+    // Add an event listener for the button
+
 }
 
 let button= document.getElementById("#nav-donate");
