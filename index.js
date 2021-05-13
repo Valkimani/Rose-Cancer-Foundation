b6a473f57ce291ba85ce6e50ce8d9aeec0ff0c9e
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-05-13 17:47:54 -0400

    Added stripe js reference for the donate button.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // added an immediate executing function
-function paymentMethod (){
+(function paymentMethod (){
     // Takes in publishable key
     const stripe = stripe('pk_test_51Iq55tDcRN3IA7cEHllozwTPRlBlFDC1c9ipzOew8gHLKSBRCDxoT4lhhm3ndTJOK1zYaoZFgN5PFAHVuJvL6JSJ00siu7wyls');
     
@@ -7,11 +7,24 @@ function paymentMethod (){
     const errorMessage = document.getElementById('error-message');
 
     // Add an event listener for the button
+checkoutButton.addEventListener('click',()=>{
+    // the function calls stripe
+    stripe.redirectTocheckout({
+        // Items represents what the user will be contributing/charged
+        items:[{price: 'price_1Iq5BODcRN3IA7cEskqYfcvZ', quantity: 1}],
+        // notify the user their donation was successful
+        successUrl: `${window.location.origin}`/success.html,
+        // if they cancel, they are redirected to the original page.
+        cancelUrl: window.location.origin,
+    })
+    .then(result => {
+        if(result.error){
+            errorMessage.textContent = result.error.message;
+        }
+    });
+});
+})();
 
-}
-
-let button= document.getElementById("#nav-donate");
-button.addEventListener("click", paymentMethod)
 
 
 
