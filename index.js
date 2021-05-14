272435402c0765769578de0d6446ca9fe70e08d8
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-05-14 09:16:29 -0400

    Footer is fianlly at the bottom of the page.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // added an immediate executing function
 (function paymentMethod (){
     // Takes in publishable key
-    const stripe = stripe('pk_test_51Iq55tDcRN3IA7cEHllozwTPRlBlFDC1c9ipzOew8gHLKSBRCDxoT4lhhm3ndTJOK1zYaoZFgN5PFAHVuJvL6JSJ00siu7wyls');
+    var stripe = Stripe('pk_test_51Iq55tDcRN3IA7cEHllozwTPRlBlFDC1c9ipzOew8gHLKSBRCDxoT4lhhm3ndTJOK1zYaoZFgN5PFAHVuJvL6JSJ00siu7wyls');
     
-    const checkoutButton =  document.getElementById('nav-button');
-    const errorMessage = document.getElementById('error-message');
+    var checkoutButton =  document.getElementById('nav-button');
+    var errorMessage = document.getElementById('error-message');
 
     // Add an event listener for the button
 checkoutButton.addEventListener('click',()=>{
