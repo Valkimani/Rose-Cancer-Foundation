695bad8bdd16c5a303d6304365ed6a820d090a52
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-05-25 23:49:03 -0400

    Paypal button is working. Added styling to the donate buuton.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// const donateBtn = document.getElementById('donate');
+// function donateBtn(event){
+    
+//     console.log(donate);
+// }
+// donateBt();
+document.querySelector("button").addEventListener('click', 
+function(event){
+    event.currentTarget.setAttribute("disabled", true);
+},{
+    once:true
+}
+);
+    
+
 
 
 
